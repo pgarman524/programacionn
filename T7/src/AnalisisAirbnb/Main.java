2fef892235b5dcd7811b2e2544d72811e578e29a
pgarman524 <151721693+pgarman524@users.noreply.github.com>
2024-04-02 22:44:00 +0200

    Update Main.java

--- T7/src/AnalisisAirbnb/Main.java
+++ T7/src/AnalisisAirbnb/Main.java
@@ -1,60 +1,73 @@
-package AnalisisAirbnb;
+package ProyectoUser;
 
-import AnalisisAirbnb.Services.GestionFicheroAirbnb;
-import Tema7Teoria.ProyectoAirBnB.Model.Alojamiento;
-import Tema7Teoria.proyectoQuiz.services.GestionFichero;
+import ProyectoUser.model.Usuario;
+import ProyectoUser.services.GestionFicheroUser;
+import Tema7Teoria.ProyectoTrivial.Services.GestionFichero;
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
+    /*
+    Crear un menú de login y de alta
+     */
 
     public static void main(String[] args) {
+        ArrayList<Usuario> arrTemporal = new ArrayList<>();
+        String ruta = "src/archivosTema7/users/users.txt";
+
+        GestionFicheroUser gf = new GestionFicheroUser();
+
+        gf.leerficheroUsers(ruta);
+        ArrayList<Usuario> registros = new ArrayList<>();
         int opc = 1;
 
-        do {
-            System.out.println("Aquí está la lista de acciones:");
-            System.out.println("1. Imprimir por pantalla los 20 alojamientos más baratos.");
-            System.out.println("2. Imprimir por pantalla los 20 alojamientos más caros.");
-            System.out.println("3. Preguntar al usuario por un presupuesto máximo por\n" +
-                    "noche, e imprimir por pantalla todos los alojamientos que se ciñan\n" +
-                    "a ese presupuesto.\n");
+        Scanner scan = new Scanner(System.in);
 
-            Scanner scan = new Scanner(System.in);
+
+        do {
+
+            System.out.println("1. Iniciar sesión");
+            System.out.println("2. Registrarse");
+            System.out.println("0. Salir");
+            System.out.print("Seleccione una opción: ");
             opc = scan.nextInt();
-            GestionFicheroAirbnb gb = new GestionFicheroAirbnb();
+
+
             switch (opc) {
                 case 1:
-            /*
-            0. necesito poder extraer los datos
-            1. Necesito colocar los datos en algún lugar
-            2. Cuando tenga esos datos debo ordenarlos.
-            3. mostrar los 20 primeros datos que aparezcan
-            */
-
-
-                    gb.leerYMostrar20BaratosAlojamineto("src/AnalisisAirbnb/Archivos/malaga.csv");
-                    // alojamientos = gb.leerFicheroAlojamineto("src/AnalisisAirbnb/Archivos/malaga.csv");
-
+                    System.out.print("Ingrese su usuario: ");
+                    String username = scan.next();
+                    System.out.print("Ingrese su contraseña: ");
+                    String password = scan.next();
+                    if (gf.leerficheroUsers(ruta).get(0).equals(username) && gf.leerficheroUsers(ruta).get(0).equals(password)) {
+                        System.out.println("Inicio de sesión exitoso.");
+                    } else {
+                        System.out.println("Nombre de usuario o contraseña incorrectos");
+                    }
                     break;
                 case 2:
+                    System.out.print("Ingrese un nombre de usuario: ");
+                    String nuevoUsuario = scan.next();
+                    if (!gf.leerficheroUsers(ruta).get(0).equals(nuevoUsuario)){
+                    System.out.println("Ingrese un nombre propio: ");
+                    String nuevoNombre = scan.next();
+                    System.out.print("Ingrese una contraseña: ");
+                    String nuevaContraseña = scan.next();
+                    Usuario usuario = new Usuario(nuevoUsuario, nuevoNombre, nuevaContraseña, false);
 
-                    gb.leerYMostrar20CarosAlojamineto("src/AnalisisAirbnb/Archivos/malaga.csv");
+                    gf.escribirFichero(registros, ruta);
+                    System.out.println("Usuario registrado ");
+                }
                     break;
 
-                case 3:
-                    gb.obtenerPresupuesto("src/AnalisisAirbnb/Archivos/malaga.csv");
-
-
-                    break;
-                case 4:
-gb.modificarPrecioAlojamiento();
-
-                    break;
             }
 
         } while (opc != 0);
 
+
     }
+
+
 }
