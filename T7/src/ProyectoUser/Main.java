d688fb5f0913c1f0e46b0acb06eaf546e2e659a4
pgarman524 <151721693+pgarman524@users.noreply.github.com>
2024-04-03 14:55:31 +0200

    Add files via upload

--- T7/src/ProyectoUser/Main.java
+++ T7/src/ProyectoUser/Main.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 public class Main {
 
     /*
-    Crear un menú de login y de alta
+    Crear un menú de login / de alta / modificar usuarios / Baja usuarios
      */
 
     public static void main(String[] args) {
@@ -20,23 +20,100 @@ public class Main {
         GestionFicheroUser gf = new GestionFicheroUser();
 
         gf.leerficheroUsers(ruta);
+        ArrayList<Usuario> registros = gf.leerficheroUsers(ruta);
 
         int opc = 1;
 
         Scanner scan = new Scanner(System.in);
 
-        opc = scan.nextInt();
 
         do {
-        switch (opc){
-            case 1:
 
-                break;
-            case 2:
+            System.out.println("1. Iniciar sesión");
+            System.out.println("2. Registrarse");
+            System.out.println("0. Salir");
+            System.out.print("Seleccione una opción: ");
+            opc = scan.nextInt();
+            String username = "";
+            String password = "";
 
-                break;
+            switch (opc) {
+                case 1:
+                    System.out.print("Ingrese su usuario: ");
+                    username = scan.next();
+                    System.out.print("Ingrese su contraseña: ");
+                    password = scan.next();
+                    try {
+                        for (int i = 0; i < registros.size(); i++) {
 
-        }
+
+                            if (registros.get(i).getId().equalsIgnoreCase(username) && registros.get(i).getPassword().equalsIgnoreCase(password)) {
+                                System.out.println("Inicio de sesión exitoso.");
+                                break;
+                            } else {
+                                System.out.println("Nombre de usuario o contraseña incorrectos");
+                            }
+
+
+                        }
+                    } catch (NullPointerException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case 2:
+                    System.out.print("Ingrese un nombre de usuario: ");
+                    String nuevoUsuario = scan.next();
+                    try {
+                        if (!username.equals(gf.leerficheroUsers(ruta).get(0).toString()) || gf.leerficheroUsers(ruta) == null) {
+                            System.out.println("Ingrese un nombre propio: ");
+                            String nuevoNombre = scan.next();
+                            System.out.print("Ingrese una contraseña: ");
+                            String nuevaContraseña = scan.next();
+                            System.out.println("Es admin(s/n): ");
+                            String isAdmin = scan.next();
+                            if (isAdmin.equalsIgnoreCase("s") || isAdmin.equalsIgnoreCase("n")) {
+                                if (isAdmin.equalsIgnoreCase("s")) {
+                                    Usuario usuario = new Usuario(nuevoUsuario, nuevoNombre, nuevaContraseña, true);
+                                    for (int i = 0; i < registros.size(); i++) {
+                                        registros.add(usuario);
+                                        gf.escribirFichero(registros, ruta);
+                                        break;
+                                    }
+                                } else {
+                                    Usuario usuario = new Usuario(nuevoUsuario, nuevoNombre, nuevaContraseña, false);
+                                    for (int i = 0; i < registros.size(); i++) {
+                                        registros.add(usuario);
+                                        gf.escribirFichero(registros, ruta);
+                                        break;
+                                    }
+                                }
+                            } else {
+                                System.out.println("Introduzca solo -> s/n");
+                            }
+                            gf.escribirFichero(registros, ruta);
+                            System.out.println("Usuario registrado ");
+                        }
+                    } catch (NullPointerException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case 3:
+                    System.out.println("Hola, ¿qué user quiere modificar?: ");
+                    String userAEliminar = scan.next();
+
+                    if (userAEliminar.equalsIgnoreCase(gf.leerficheroUsers(ruta).get(0).toString())) {
+                        //   registros.remove(i);
+                        gf.escribirFichero(registros, ruta);
+
+                    }
+
+                    break;
+                case 4:
+                    System.out.println("Hola, indique el usuario que quiere dar de baja: ");
+
+
+                    break;
+            }
 
         } while (opc != 0);
 
---
+++ T7/src/ProyectoUser/services/Services.java
@@ -0,0 +1,46 @@
+package ProyectoUser.services;
+
+import ProyectoUser.model.Usuario;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Services {
+    ArrayList<Usuario> arrTemporal = new ArrayList<>();
+    String ruta = "src/archivosTema7/users/users.txt";
+
+    GestionFicheroUser gf = new GestionFicheroUser();
+
+
+    Scanner scan = new Scanner(System.in);
+
+
+    public void loginUser(){
+
+        gf.leerficheroUsers(ruta);
+        ArrayList<Usuario> registros = gf.leerficheroUsers(ruta);
+
+        int opc = 1;
+
+        System.out.print("Ingrese su usuario: ");
+        String username = scan.next();
+        System.out.print("Ingrese su contraseña: ");
+        String password = scan.next();
+        try {
+            for (int i = 0; i < registros.size(); i++) {
+
+                if (registros.get(i).getId().equalsIgnoreCase(username) && registros.get(i).getPassword().equalsIgnoreCase(password)) {
+                    System.out.println("Inicio de sesión exitoso.");
+                    System.out.println("");
+                } else {
+                    System.out.println("Nombre de usuario o contraseña incorrectos");
+                    System.out.println("");
+                }
+            }
+
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
