9f2d9e9da35214d3c67dfc50efc1d114cb0e3d91
pgarman524 <151721693+pgarman524@users.noreply.github.com>
2024-05-20 14:58:19 +0200

    Add files via upload

--- ProyectoLoginFinal/build.gradle.kts
+++ ProyectoLoginFinal/build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.mysql:mysql-connector-j:8.4.0")
 }
 
 tasks.test {
